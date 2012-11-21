ff5fe929abb976918f882a3a65ec8379476a1d26
Erik Kargard <erik@rightscale.com>
2012-11-21 20:32:13 +0000

    Cookbooks override initialized

---
+++ cookbooks/web_apache/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
+description      "Installs/configures the apache2 webserver"
+version          "0.0.1"
+
+depends "apache2"
+
