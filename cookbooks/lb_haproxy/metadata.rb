17261cbc097c64d929ed2a707b52351ddcef2fc8
Steve Cassidy <steve@two-bulls.com>
2013-03-25 10:18:17 +1100

    Remove unused cookbook entries.

--- cookbooks/lb_haproxy/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "RightScale, Inc."
-maintainer_email "support@rightscale.com"
-license          "All rights reserved"
-description      "Override RightScale HAProxy configuration"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
-
-depends "rs_utils"
-
--- cookbooks/web_apache/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "RightScale, Inc."
-maintainer_email "support@rightscale.com"
-license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
-description      "Installs/configures the apache2 webserver"
-version          "0.0.1"
-
-depends "apache2"
-
