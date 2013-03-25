df5b4671cf19ff5b58e00cbc39a31e31c3f548a1
Steve Cassidy <steve@two-bulls.com>
2013-03-25 16:33:12 +1100

    I'm starting to dislike rightscale quite a lot.

--- cookbooks/postgis/recipes/add_geo_template.rb
+++ cookbooks/postgis/recipes/add_geo_template.rb
@@ -4,12 +4,11 @@ subfolder = "/contrib/postgis-1.5"
 template = node[:postgis][:template_name]
 execute 'create_postgis_template' do
   not_if "su postgres -c 'psql -qAt --list' | grep -q '^#{node[:postgis][:template_name]}\|'"
-  user 'postgres'
   command <<CMD
-(createdb -E UTF8 #{template} -T template0) &&
-(psql -d #{template} -c "UPDATE pg_database SET datistemplate='true' WHERE datname='#{template}';") &&
-(psql -d #{template} -f `pg_config --sharedir`#{subfolder}/postgis.sql) &&
-(psql -d #{template} -f `pg_config --sharedir`#{subfolder}/spatial_ref_sys.sql)
+(su postgres -c 'createdb -E UTF8 #{template} -T template0') &&
+(su postgres -c 'psql -d #{template} -c "UPDATE pg_database SET datistemplate=\'true\' WHERE datname=\'#{template}\';"') &&
+(su postgres -c 'psql -d #{template} -f `pg_config --sharedir`#{subfolder}/postgis.sql') &&
+(su postgres -c 'psql -d #{template} -f `pg_config --sharedir`#{subfolder}/spatial_ref_sys.sql')
 CMD
   action :run
 end
