562937591626e1607d51558cc367424efa09e11a
Steve Cassidy <steve@two-bulls.com>
2013-03-25 17:34:26 +1100

    Retrying without update command

--- cookbooks/postgis/recipes/add_geo_template.rb
+++ cookbooks/postgis/recipes/add_geo_template.rb
@@ -6,7 +6,6 @@ execute 'create_postgis_template' do
   not_if "su postgres -c 'psql -qAt --list' | grep -q '^#{node[:postgis][:template_name]}\|'"
   command <<CMD
 (su postgres -c "createdb -E UTF8 #{template} -T template0") &&
-(su postgres -c "psql -d #{template} -c \"UPDATE pg_database SET datistemplate='true' WHERE datname='#{template}';\"") &&
 (su postgres -c "psql -d #{template} -f `pg_config --sharedir`#{subfolder}/postgis.sql") &&
 (su postgres -c "psql -d #{template} -f `pg_config --sharedir`#{subfolder}/spatial_ref_sys.sql")
 CMD
