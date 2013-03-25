7c1955841c160ce2a36b82a7d24186893e59b551
Steve Cassidy <steve@two-bulls.com>
2013-03-25 16:09:45 +1100

    Add rightscale dependancy for marker block.

--- cookbooks/postgis/metadata.rb
+++ cookbooks/postgis/metadata.rb
@@ -5,6 +5,7 @@ description      "Installs and configures Postgis 1.5 onto Postgresql 9.1"
 version          "1.0.0"
 
 # supports "centos", "~> 5.8", "~> 6"
+depends "rightscale"
 
 recipe "postgis::default",
   "Does nothing"
