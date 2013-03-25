7be6e806f9e1e83bacd4d575220b22f4927372a1
Steve Cassidy <steve@two-bulls.com>
2013-03-25 16:23:26 +1100

    Fixed geo script

--- cookbooks/postgis/recipes/add_geo_template.rb
+++ cookbooks/postgis/recipes/add_geo_template.rb
@@ -3,10 +3,10 @@ rightscale_marker :begin
 subfolder = "/contrib/postgis-1.5"
 template = node[:postgis][:template_name]
 execute 'create_postgis_template' do
-  not_if "psql -qAt --list | grep -q '^#{node[:postgis][:template_name]}\|'", :user => 'postgres'
+  not_if "su postgres -c 'psql -qAt --list' | grep -q '^#{node[:postgis][:template_name]}\|'"
   user 'postgres'
   command <<CMD
-(createdb -E UTF8 #{node[:postgis][:template_name]} -T template0) &&
+(createdb -E UTF8 #{template} -T template0) &&
 (psql -d #{template} -c "UPDATE pg_database SET datistemplate='true' WHERE datname='#{template}';") &&
 (psql -d #{template} -f `pg_config --sharedir`#{subfolder}/postgis.sql) &&
 (psql -d #{template} -f `pg_config --sharedir`#{subfolder}/spatial_ref_sys.sql)
