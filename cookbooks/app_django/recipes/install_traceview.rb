27b33745307e7f373e694c3740f3f734b111787f
Steve Cassidy <steve@two-bulls.com>
2013-04-08 13:35:44 +1000

    Add apache package to install for traceview

--- cookbooks/app_django/recipes/install_traceview.rb
+++ cookbooks/app_django/recipes/install_traceview.rb
@@ -2,9 +2,19 @@
 #sh ./install_tracelytics.sh [access key]
 sh_file = "install_tracelytics.sh"
 
+# get client
 remote_file "/tmp/#{sh_file}}" do
  source "https://www.tracelytics.com/#{sh_file}" 
  mode "0644"
 end
 
+# run client
 execute "sh /tmp/#{sh_file} #{node[:app_django][:trace_view_key]}"
+
+# install apache library
+package "libapache2-mod-oboe" do
+  action :install
+end
+
+# restart apache
+action_restart
