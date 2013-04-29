006330112f20acbaeef4cfe9cba0fc0e3d4e8542
Steve Cassidy <steve@two-bulls.com>
2013-04-29 22:57:35 +1000

    Bug fix to script

--- cookbooks/lb_haproxy/files/default/haproxy-cat.sh
+++ cookbooks/lb_haproxy/files/default/haproxy-cat.sh
@@ -43,6 +43,7 @@ done
 
 #ssl
 cat /etc/haproxy/haproxy.ssl >> ${CONF_FILE}
+echo "" >> ${CONF_FILE}
 if [ $(ls -1A /etc/haproxy/ssl | wc -l) -gt 0 ]; then
   cat /etc/haproxy/ssl/* >> ${CONF_FILE}
 fi
