d933f611fa4c095e6362d483cb49121701be9432
Steve Cassidy <steve@two-bulls.com>
2013-05-09 12:14:47 +1000

    Fix issue with path naming

--- cookbooks/soundhalo_sites/recipes/default.rb
+++ cookbooks/soundhalo_sites/recipes/default.rb
@@ -11,7 +11,7 @@ rightscale_marker :begin
 
 node[:soundhalo_sites][:app].each do |app_name, entry|
   node[:soundhalo_sites][:app][app_name][:web_dir] = ::File.join(node[:soundhalo_sites][:web_root],app_name)
-  node[:soundhalo_sites][:app][app_name][:git_dir] = ::File.join(node[:soundhalo_sites][:web_root],app_name,".git")
+  node[:soundhalo_sites][:app][app_name][:git_dir] = ::File.join(node[:soundhalo_sites][:web_root],"#{app_name}.git")
 end
 
 directory "#{node[:soundhalo_sites][:web_root]}" do
