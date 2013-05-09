47966adffb94addc8fc0b0b10cc3e8e812faa40d
Steve Cassidy <steve@two-bulls.com>
2013-05-09 11:52:22 +1000

    Refactor how node is installed

--- cookbooks/soundhalo_sites/recipes/default.rb
+++ cookbooks/soundhalo_sites/recipes/default.rb
@@ -14,4 +14,9 @@ node[:soundhalo_sites][:app].each do |app_name, entry|
   node[:soundhalo_sites][:app][app_name][:git_dir] = ::File.join(node[:soundhalo_sites][:web_root],app_name,".git")
 end
 
+directory "#{node[:soundhalo_sites][:web_root]}" do
+  action :create
+  recursive true
+end
+
 rightscale_marker :end
--- cookbooks/soundhalo_sites/recipes/install_nodeJS.rb
+++ cookbooks/soundhalo_sites/recipes/install_nodeJS.rb
@@ -1,28 +1,18 @@
 rightscale_marker :begin
 
 # install node dependancies
-packages = ['g++','curl','libssl-dev','apache2-utils','git-core']
+packages = ['g++','curl','libssl-dev','apache2-utils','git-core','python-software-properties','python','make']
 packages.each do |p|
   package p do
     action :install
   end
 end
 
-directory "#{node[:soundhalo_sites][:web_root]}" do
-  action :create
-  recursive true
-end
+# add special repo source
+execute "add-apt-repository -y ppa:chris-lea/node.js && apt-get update"
 
-bash "install nodejs" do
-  flags "-ex"
-  code <<-EOH
-    cd #{node[:soundhalo_sites][:web_root]}
-    git clone git://github.com/ry/node.git node
-    cd node
-    ./configure
-    make
-    su -c 'make install'
-  EOH
+package "nodejs" do
+  action :install
 end
 
 rightscale_marker :end
