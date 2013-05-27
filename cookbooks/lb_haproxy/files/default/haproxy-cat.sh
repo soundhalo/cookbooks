7dac5e0cca5d0cdabf34d75599bc394ab59a8e98
Steve Cassidy <steve@two-bulls.com>
2013-05-27 17:02:42 +1000

    Make haproxy listen on 80. Stop using apache infront

--- cookbooks/lb_haproxy/files/default/haproxy-cat.sh
+++ cookbooks/lb_haproxy/files/default/haproxy-cat.sh
@@ -13,7 +13,10 @@ CONF_FILE=/etc/haproxy/haproxy.cfg
 
 cat /etc/haproxy/haproxy.cfg.head > ${CONF_FILE}
 
-echo "frontend all_requests 127.0.0.1:85" >> ${CONF_FILE}
+echo "frontend all_requests" >> ${CONF_FILE}
+echo "  bind 0.0.0.0:80" >> ${CONF_FILE}
+echo "  timeout client 5000" >> ${CONF_FILE}
+echo "  option http-server-close" >> ${CONF_FILE}
 echo "  option forwardfor" >> ${CONF_FILE}
 
 pools=""
