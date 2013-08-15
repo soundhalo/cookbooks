e7b33ae687812b78e3975c391be6db00092de977
Steve Cassidy <steve@two-bulls.com>
2013-08-15 22:09:29 +1000

    Add load balancer recipes

--- cookbooks/soundhalo_sites/metadata.rb
+++ cookbooks/soundhalo_sites/metadata.rb
@@ -28,6 +28,30 @@ recipe "soundhalo_sites::update_code_desktop",
 recipe "soundhalo_sites::update_code_mobile",
   "checkout or pull down code for mobile app. runs build script and restarts apache"
 
+recipe "app::do_loadbalancers_allow",
+  "Allows connections from all load balancers within a given listener pool" +
+  " which are tagged with loadbalancer:lb=<applistener_name>." +
+  " This script should be run on an application server before it makes" +
+  " a request to be connected to the load balancers."
+
+recipe "app::do_loadbalancers_deny",
+  "Denies connections from all load balancers which are tagged with" +
+  " loadbalancer:lb=<applistener_name>. For example, you can run this" +
+  " script on an application server to deny connections from all load" +
+  " balancers within a given listener pool."
+
+recipe "app::request_loadbalancer_allow",
+  "Sends a request to all application servers tagged with" +
+  " loadbalancer:app=<applistener_name> to allow connections from the server's" +
+  " private IP address. This script should be run on a load balancer before" +
+  " any application servers are attached to it."
+
+recipe "app::request_loadbalancer_deny",
+  "Sends a request to all application servers tagged with" +
+  " loadbalancer:app=<applistener_name> to deny connections from the server's" +
+  " private IP address. This script should be run on a load balancer after" +
+  " disconnecting application servers or upon decommissioning."
+    
 #recipe "soundhalo_sites::update_code_www",
 #  "checkout or pull down code for www site."
 
